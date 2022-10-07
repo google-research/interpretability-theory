fdd098c4f7460d8b968cef31686ab2914bb453a5
Blair Bilodeau <blair_bilodeau@hotmail.com>
2022-10-07 12:27:28 -0700

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -15,122 +15,138 @@ Currently, visualization is done in-line in a colab by default, but will save to
 a filename if passed one.
 """
 
+import dataclasses
 from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+@dataclasses.dataclass
+class ExperimentParams:
+  """Keeps track of metadata from experiments.
+
+  Each entry is a piece of metadata which is an array, where each entry of the
+  array corresponds to the data from a single experiment.
+  """
+  n_shap_true_negative: np.ndarray = np.array([])
+  n_lime_true_negative: np.ndarray = np.array([])
+  n_intgrad_true_negative: np.ndarray = np.array([])
+  n_grad_true_negative: np.ndarray = np.array([])
+  n_shap_true_positive: np.ndarray = np.array([])
+  n_lime_true_positive: np.ndarray = np.array([])
+  n_intgrad_true_positive: np.ndarray = np.array([])
+  n_grad_true_positive: np.ndarray = np.array([])
+  n_oracle_positive: np.ndarray = np.array([])
+  n_experiment_samples: np.ndarray = np.array([])
+
+  def reset(self, n_repetitions: int) -> None:
+    # reset global variables to store experiment metadata
+    for field in self.__dataclass_fields__:
+      setattr(self, field, np.zeros(n_repetitions))
+
+
 class Experiment():
   """Object to run interpretability experiments."""
+  features_data: np.ndarray
+  labels_data: np.ndarray
+  n_features: int
+  n_classes: int
+  n_data: int
+  experiment_params: ExperimentParams
 
   def __init__(self) -> None:
-    self.features_data = None
-    self.labels_data = None
-    self.n_features = None
-    self.n_classes = None
-    self.n_data = None
-    self.feature_names = None
-
-    # output 0 when oracle is 0 (specificity, or true negative)
-    self.n_shap_true_negative = None
-    self.n_lime_true_negative = None
-    self.n_intgrad_true_negative = None
-    self.n_grad_true_negative = None
-    # output 1 when oracle is 1 (sensitivity, or true positive)
-    self.n_shap_true_positive = None
-    self.n_lime_true_positive = None
-    self.n_intgrad_true_positive = None
-    self.n_grad_true_positive = None
-    # how often oracle is 1
-    self.n_oracle_positive = None
-
-    # counters for experiments
-    self.n_experiment_samples = None
-    self.model_accuracy = None
+    self.experiment_params = ExperimentParams()
 
   def _sensitivity(self, n_true_positive: np.ndarray) -> np.ndarray:
     """Computes the sensitivity (a probability).
 
     Args:
-      n_true_positive: number of times a positive oracle was correctly
-        identified
+      n_true_positive: each element of the array is the number of times a
+        positive oracle was correctly identified for that experiment
 
     Returns:
-      number of true positives divided by total number of positives
+      each element of the array is the number of true positives divided by total
+      number of positives for that experiment
     """
-    return np.array(
-        list(
-            map(lambda x: round(x, 3),
-                n_true_positive / self.n_oracle_positive)))
+    return np.array([
+        round(x, 3)
+        for x in n_true_positive / self.experiment_params.n_oracle_positive
+    ])
 
   def _specificity(self, n_true_negative: np.ndarray) -> np.ndarray:
     """Computes the sprecificity (a probability).
 
     Args:
-      n_true_negative: number of times a negative oracle was correctly
-        identified
+      n_true_negative: each element of the array is the number of times a
+        negative oracle was correctly identified for that experiment
 
     Returns:
-      number of true negatives divided by total number of negatives
+      each element of the array is the number of true negatives divided by total
+      number of negatives for that experiment
     """
-    return np.array(
-        list(
-            map(
-                lambda x: round(x, 3), n_true_negative /
-                (self.n_experiment_samples - self.n_oracle_positive))))
+    return np.array([
+        round(x, 3) for x in n_true_negative /
+        (self.experiment_params.n_experiment_samples -
+         self.experiment_params.n_oracle_positive)
+    ])
 
   def _accuracy(self, n_true_positive: np.ndarray,
                 n_true_negative: np.ndarray) -> np.ndarray:
-    """Computes the accuracy (a probability).
+    """Computes the accuracy (a probability) for each experiment.
 
     Args:
-      n_true_positive: number of times a positive oracle was correctly
-        identified
-      n_true_negative: number of times a negative oracle was correctly
-        identified
+      n_true_positive: each element of the array is the number of times a
+        positive oracle was correctly identified for that experiment
+      n_true_negative: each element of the array is the number of times a
+        negative oracle was correctly identified for that experiment
 
     Returns:
-      number of true negatives or true positives divided by total number of
-      observations
+      each element of the array is the number of true negatives or true
+      positives divided by total number of
+      observations for that experiment
     """
-    return np.array(
-        list(
-            map(lambda x: round(x, 3), (n_true_positive + n_true_negative) /
-                self.n_experiment_samples)))
+    return np.array([
+        round(x, 3) for x in (n_true_positive + n_true_negative) /
+        self.experiment_params.n_experiment_samples
+    ])
 
   def visualize_experiment_table(self) -> None:
-    """Print out the specificity, sensitivity, and accuracy of the test."""
+    """Print out the specificity, sensitivity, and accuracy for each experiment.
+    """
     print('Pr(shap(f,x)=1 | oracle(f,x)=1) = {x}'.format(
-        x=self._sensitivity(self.n_shap_true_positive)))
+        x=self._sensitivity(self.experiment_params.n_shap_true_positive)))
     print('Pr(lime(f,x)=1 | oracle(f,x)=1) = {x}'.format(
-        x=self._sensitivity(self.n_lime_true_positive)))
+        x=self._sensitivity(self.experiment_params.n_lime_true_positive)))
     print('Pr(IG(f,x)=1 | oracle(f,x)=1) = {x}'.format(
-        x=self._sensitivity(self.n_intgrad_true_positive)))
+        x=self._sensitivity(self.experiment_params.n_intgrad_true_positive)))
     print('Pr(grad(f,x)=1 | oracle(f,x)=1) = {x}'.format(
-        x=self._sensitivity(self.n_grad_true_positive)))
+        x=self._sensitivity(self.experiment_params.n_grad_true_positive)))
     print('Pr(shap(f,x)=0 | oracle(f,x)=0) = {x}'.format(
-        x=self._specificity(self.n_shap_true_negative)))
+        x=self._specificity(self.experiment_params.n_shap_true_negative)))
     print('Pr(lime(f,x)=0 | oracle(f,x)=0) = {x}'.format(
-        x=self._specificity(self.n_lime_true_negative)))
+        x=self._specificity(self.experiment_params.n_lime_true_negative)))
     print('Pr(IG(f,x)=0 | oracle(f,x)=0) = {x}'.format(
-        x=self._specificity(self.n_intgrad_true_negative)))
+        x=self._specificity(self.experiment_params.n_intgrad_true_negative)))
     print('Pr(grad(f,x)=0 | oracle(f,x)=0) = {x}'.format(
-        x=self._specificity(self.n_grad_true_negative)))
+        x=self._specificity(self.experiment_params.n_grad_true_negative)))
     print('Pr(shap(f,x) matches oracle(f,x)) = {x}'.format(
-        x=self._accuracy(self.n_shap_true_positive, self.n_shap_true_negative)))
+        x=self._accuracy(self.experiment_params.n_shap_true_positive,
+                         self.experiment_params.n_shap_true_negative)))
     print('Pr(lime(f,x) matches oracle(f,x)) = {x}'.format(
-        x=self._accuracy(self.n_lime_true_positive, self.n_lime_true_negative)))
+        x=self._accuracy(self.experiment_params.n_lime_true_positive,
+                         self.experiment_params.n_lime_true_negative)))
     print('Pr(IG(f,x) matches oracle(f,x)) = {x}'.format(
-        x=self._accuracy(self.n_intgrad_true_positive,
-                         self.n_intgrad_true_negative)))
+        x=self._accuracy(self.experiment_params.n_intgrad_true_positive,
+                         self.experiment_params.n_intgrad_true_negative)))
     print('Pr(grad(f,x) matches oracle(f,x)) = {x}'.format(
-        x=self._accuracy(self.n_grad_true_positive, self.n_grad_true_negative)))
+        x=self._accuracy(self.experiment_params.n_grad_true_positive,
+                         self.experiment_params.n_grad_true_negative)))
     print('Pr(oracle(f,x)=1) = {x}'.format(
-        x=np.array(
-            list(
-                map(lambda x: round(x, 3), self.n_oracle_positive /
-                    self.n_experiment_samples)))))
+        x=np.array([
+            round(x, 3) for x in self.experiment_params.n_oracle_positive /
+            self.experiment_params.n_experiment_samples
+        ])))
 
   def visualize_hypothesis_test(self,
                                 title: str,
@@ -149,16 +165,16 @@ class Experiment():
     ax.set(xlabel='Pr(True Positive)', ylabel='Pr(True Negative)', title=title)
 
     sensitivities = np.array([
-        self._sensitivity(self.n_shap_true_positive),
-        self._sensitivity(self.n_intgrad_true_positive),
-        self._sensitivity(self.n_lime_true_positive),
-        self._sensitivity(self.n_grad_true_positive)
+        self._sensitivity(self.experiment_params.n_shap_true_positive),
+        self._sensitivity(self.experiment_params.n_intgrad_true_positive),
+        self._sensitivity(self.experiment_params.n_lime_true_positive),
+        self._sensitivity(self.experiment_params.n_grad_true_positive)
     ])
     specificities = np.array([
-        self._specificity(self.n_shap_true_negative),
-        self._specificity(self.n_intgrad_true_negative),
-        self._specificity(self.n_lime_true_negative),
-        self._specificity(self.n_grad_true_negative)
+        self._specificity(self.experiment_params.n_shap_true_negative),
+        self._specificity(self.experiment_params.n_intgrad_true_negative),
+        self._specificity(self.experiment_params.n_lime_true_negative),
+        self._specificity(self.experiment_params.n_grad_true_negative)
     ])
 
     colors = ['blue', 'orange', 'green', 'red']
@@ -181,14 +197,17 @@ class Experiment():
     ax = plt.subplots()[1]
     ax.set_ylim([-0.05, 1.05])
 
-    n_repetitions = len(self.n_experiment_samples)
+    n_repetitions = len(self.experiment_params.n_experiment_samples)
 
     accuracies = np.array([
-        self._accuracy(self.n_shap_true_positive, self.n_shap_true_negative),
-        self._accuracy(self.n_intgrad_true_positive,
-                       self.n_intgrad_true_negative),
-        self._accuracy(self.n_lime_true_positive, self.n_lime_true_negative),
-        self._accuracy(self.n_grad_true_positive, self.n_grad_true_negative)
+        self._accuracy(self.experiment_params.n_shap_true_positive,
+                       self.experiment_params.n_shap_true_negative),
+        self._accuracy(self.experiment_params.n_intgrad_true_positive,
+                       self.experiment_params.n_intgrad_true_negative),
+        self._accuracy(self.experiment_params.n_lime_true_positive,
+                       self.experiment_params.n_lime_true_negative),
+        self._accuracy(self.experiment_params.n_grad_true_positive,
+                       self.experiment_params.n_grad_true_negative)
     ])
     mean_accuracies = np.mean(accuracies, axis=1)
     std_accuracies = 1.96 * np.std(accuracies, axis=1) / np.sqrt(n_repetitions)
@@ -201,10 +220,11 @@ class Experiment():
     plt.xticks(xvals, xlabels)
     ax.set_xlim([np.min(xvals) - 0.55, np.max(xvals) + 0.55])
 
-    trivial_accuracy = np.max(
-        (self.n_oracle_positive / self.n_experiment_samples, 1 -
-         (self.n_oracle_positive / self.n_experiment_samples)),
-        axis=0)
+    trivial_accuracy = np.max((self.experiment_params.n_oracle_positive /
+                               self.experiment_params.n_experiment_samples, 1 -
+                               (self.experiment_params.n_oracle_positive /
+                                self.experiment_params.n_experiment_samples)),
+                              axis=0)
     mean_trivial_accuracy = np.mean(trivial_accuracy)
     std_trivial_accuracy = 1.96 * np.std(trivial_accuracy) / np.sqrt(
         n_repetitions)
@@ -230,5 +250,5 @@ class Experiment():
     ax.set(xlabel='', ylabel='Accuracy', title=title)
 
     if filepath is not None:
-      with open(filepath + '.png', 'wb') as f:
+      with Open(filepath + '.png', 'wb') as f:
         plt.savefig(f, format='png')
